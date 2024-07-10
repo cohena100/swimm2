776bcf0c4e8dfc2861223e6ebf1767fb865a22bf
Avi Cohen <cohena100@gmail.com>
2024-07-10 15:47:26 +0300

    cleanup

--- src/lib/api/posts.ts
+++ src/lib/api/posts.ts
@@ -1,6 +1,8 @@
 import { FETCH_POSTS_LIMIT } from "../constants";
 
 export const fetchPosts = async ({ pageParam = 1 }: { pageParam: unknown }) => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${pageParam}&_limit=${FETCH_POSTS_LIMIT}`);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_page=${pageParam}&_limit=${FETCH_POSTS_LIMIT}`,
+  );
   return response.json();
 };
--- src/lib/api/users.ts
+++ src/lib/api/users.ts
@@ -1,35 +1,51 @@
-import { FETCH_USERS_LIMIT, FETCH_COMMENTS_LIMIT, FETCH_POSTS_LIMIT, FETCH_ALBUMS_LIMIT, FETCH_PHOTOS_LIMIT } from "../constants";
+import {
+  FETCH_USERS_LIMIT,
+  FETCH_COMMENTS_LIMIT,
+  FETCH_POSTS_LIMIT,
+  FETCH_ALBUMS_LIMIT,
+  FETCH_PHOTOS_LIMIT,
+} from "../constants";
 
 export const fetchUsers = async ({ pageParam = 1 }: { pageParam: unknown }) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users?_page=${pageParam}&_limit=${FETCH_USERS_LIMIT}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users?_page=${pageParam}&_limit=${FETCH_USERS_LIMIT}`,
+  );
   return response.json();
 };
 
 export const fetchUserPosts =
   (userId: string) =>
   async ({ pageParam = 1 }: { pageParam: unknown }) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts?_page=${pageParam}&_limit=${FETCH_POSTS_LIMIT}`);
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${userId}/posts?_page=${pageParam}&_limit=${FETCH_POSTS_LIMIT}`,
+    );
     return response.json();
   };
 
 export const fetchUserPostComments =
   (postId: string) =>
   async ({ pageParam = 1 }: { pageParam: unknown }) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments?_page=${pageParam}&_limit=${FETCH_COMMENTS_LIMIT}`);
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${postId}/comments?_page=${pageParam}&_limit=${FETCH_COMMENTS_LIMIT}`,
+    );
     return response.json();
   };
 
 export const fetchUserAlbums =
   (userId: string) =>
   async ({ pageParam = 1 }: { pageParam: unknown }) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/albums?_page=${pageParam}&_limit=${FETCH_ALBUMS_LIMIT}`);
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${userId}/albums?_page=${pageParam}&_limit=${FETCH_ALBUMS_LIMIT}`,
+    );
     return response.json();
   };
 
 export const fetchUserAlbumPhotos =
   (albumId: string) =>
   async ({ pageParam = 1 }: { pageParam: unknown }) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos?_page=${pageParam}&_limit=${FETCH_PHOTOS_LIMIT}`);
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/albums/${albumId}/photos?_page=${pageParam}&_limit=${FETCH_PHOTOS_LIMIT}`,
+    );
     return response.json();
   };
