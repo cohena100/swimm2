5048cc187fa4fe51cdb8e376d72795bb060e2c8f
Avi Cohen <cohena100@gmail.com>
2024-07-10 10:45:35 +0300

    Update users.ts

--- src/lib/api/users.ts
+++ src/lib/api/users.ts
@@ -1,6 +1,7 @@
 import { FETCH_USERS_LIMIT, FETCH_COMMENTS_LIMIT, FETCH_POSTS_LIMIT } from "../constants";
 
 export const fetchUsers = async ({ pageParam = 1 }: { pageParam: unknown }) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(`https://jsonplaceholder.typicode.com/users?_page=${pageParam}&_limit=${FETCH_USERS_LIMIT}`);
   return response.json();
 };
