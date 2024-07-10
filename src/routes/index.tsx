45ca267d7e6a995f92faa6e7ddb28f94cec12797
Avi Cohen <cohena100@gmail.com>
2024-07-10 10:45:27 +0300

    reresh bug

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { infiniteQueryOptions, useInfiniteQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { IUser } from "../lib/types/user";
 import User from "../components/user";
 import { fetchUsers } from "../lib/api/users";
+import Spinner from "../components/spinner";
 
 const usersQueryOptions = infiniteQueryOptions<IUser[]>({
   queryKey: ["users"],
@@ -29,12 +30,13 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   const { ref, inView } = useInView();
+  const [isRefresh, refresh] = useState(0);
   const { data, fetchNextPage, isFetchingNextPage, hasNextPage } = useInfiniteQuery(usersQueryOptions);
   useEffect(() => {
     if (inView) {
-      fetchNextPage();
+      fetchNextPage().then(() => refresh(isRefresh + 1));
     }
-  }, [inView, fetchNextPage]);
+  }, [inView, fetchNextPage, isRefresh]);
   return (
     <div className="mt-4">
       <div className="flex flex-wrap gap-4">
@@ -46,8 +48,13 @@ function Index() {
           </React.Fragment>
         ))}
       </div>
-      {isFetchingNextPage && hasNextPage ? <p className="text-center">Loading...</p> : <p className="text-center">No more users found</p>}
-      <div ref={ref}></div>
+      <div ref={ref}>
+        {(isFetchingNextPage || hasNextPage) && (
+          <div className="flex justify-center mt-4">
+            <Spinner />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
