e85d58365f511c94b3460316c7479e539d17e857
Avi Cohen <cohena100@gmail.com>
2024-07-13 10:58:16 +0300

    env

--- src/lib/api/users.ts
+++ src/lib/api/users.ts
@@ -9,7 +9,7 @@ import {
 export const fetchUsers = async ({ pageParam = 1 }: { pageParam: unknown }) => {
   // await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/users?_page=${pageParam}&_limit=${FETCH_USERS_LIMIT}`,
+    `${import.meta.env.VITE_BASE_URL}/users?_page=${pageParam}&_limit=${FETCH_USERS_LIMIT}`,
   );
   return response.json();
 };
